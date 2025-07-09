706f4db6ed1dff82795849feda8e720ebaf7fca3
Massimiliano <massimiliano@MacBook-Pro-de-Massimiliano.local>
2025-07-09 23:00:38 +0200

    Initial commit Tomatocoin

---
+++ pages/index.js
@@ -0,0 +1,67 @@
+
+import Image from 'next/image'
+import { motion } from 'framer-motion'
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-red-600 to-red-400 flex items-center justify-center px-4">
+      <motion.div
+        initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8 }}
+        className="bg-white bg-opacity-10 backdrop-blur-md p-8 rounded-2xl shadow-lg max-w-xl text-center text-white"
+      >
+                <h1 className="text-4xl font-bold mb-4">Tomatocoin 🍅</h1>
+        <p className="text-lg mb-2">
+          Discover Tomatocoin, the memecoin that refreshes your portfolio!
+        </p>
+        <p className="text-base mb-8">
+          CA: CdczQNrp2DZ9c89LSjCyRF6VmS4VtTCBkNSjXtpvmoon
+        </p>
+        <div className="flex flex-col gap-4">
+          <a
+            href="https://jup.ag/swap/SOL-CdczQNrp2DZ9c89LSjCyRF6VmS4VtTCBkNSjXtpvmoon"
+            target="_blank"
+            className="bg-white text-red-600 font-bold text-lg py-3 rounded hover:bg-gray-100 transition"
+          >
+            🚀 Buy Tomatocoins
+          </a>
+          <a
+            href="https://x.com/offtomatocoin?s=21"
+            target="_blank"
+            className="border border-white text-white text-lg py-3 rounded hover:bg-white hover:text-red-600 transition"
+          >
+            🐦 Follow on X
+          </a>
+          <a
+            href="https://t.me/tomatocoinsolana"
+            target="_blank"
+            className="border border-white text-white text-lg py-3 rounded hover:bg-white hover:text-red-600 transition"
+          >
+            💬 Join our Telegram
+          </a>
+          <a
+            href="https://dexscreener.com/solana/3ypq174goek9gg39r2eosxvc8fli3qwowkd3uacvnlsf"
+            target="_blank"
+            className="border border-white text-white text-lg py-3 rounded hover:bg-white hover:text-red-600 transition"
+          >
+            📈 Graph on Dexscreener
+          </a>
+        </div>
+	<Image
+          src="/tomato-girl.png"
+          alt="Tomato Girl"
+          width={400}
+          height={400}
+          className="mx-auto rounded-xl mb-6 mt-8"
+        />
+
+	<p className="text-base mt-4">
+  	   Tomatocoin has been created on 28 June 2025 using the Moonshot app. It ensures an high level of security. This is not financial advice, do your own research.
+	</p>
+	
+      </motion.div>
+    </div>
+
+  )
+}
